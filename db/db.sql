b0f83bf92bc35a04878ac82af844b7599a4796dc
Phước Nguyễn Văn <phuocpr1998@gmail.com>
2019-10-25 23:45:27 +0700

    udpate sql script

--- db/db.sql
+++ db/db.sql
@@ -3,10 +3,10 @@ CREATE TABLE USERS
 	email varchar(50) PRIMARY KEY,
 	password varchar(50),
 	name varchar(50) character set utf8 not null,
-	birthday date not null,
+	birthday date,
 	photo varchar(100),
-    googleId int,
-    facebookId int,
+    googleId varchar(50)  ,
+    facebookId varchar(50) ,
     loginType varchar(10) not null,
     point int DEFAULT 0
 );
@@ -20,3 +20,4 @@ CREATE TABLE GAMEHISTORY
     FOREIGN KEY (user1) REFERENCES USERS(email),
     FOREIGN KEY (user2) REFERENCES USERS(email)
 )
+
