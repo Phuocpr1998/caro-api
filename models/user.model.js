24b4344eb64598e5815a1d240b41e1cb2210898c
Phước Nguyễn Văn <phuocpr1998@gmail.com>
2019-10-26 21:19:40 +0700

    update info for login facebook and google

--- models/user.model.js
+++ models/user.model.js
@@ -7,7 +7,13 @@ module.exports = {
     findOneEmail: (user) => {
         return db.load(`select * from USERS where email = '${user.email}'`);
     },
+    findOneEmailAndNotHaveLoginType: (user, loginType) => {
+        return db.load(`select * from USERS where email = '${user.email}' and loginType <> '${loginType}'`);
+    },
     add: (user) => {
         return db.add(`USERS`, user);
+    },
+    udpate: (user) => {
+        return db.update(`USERS`, user, 'email');
     }
 }
--- routes/user/user.js
+++ routes/user/user.js
@@ -13,13 +13,13 @@ router.post('/login', passport.authenticate('local', { session: false }), functi
     res.json({ token: jwt.sign(Object.assign({}, req.user), secret_key) });
 });
 
-router.get('/login-google', passport.authenticate('google', {  session: false, scope: ['profile', 'email'] }));
+router.get('/login-google', passport.authenticate('google', { session: false, scope: ['profile', 'email'] }));
 router.get('/login-google/callback',
     passport.authenticate('google', {
         session: false
     }), function (req, res, next) {
         if (!req.user) {
-            res.status(401).json({message: "Login with google fail"});
+            res.status(401).json({ message: "Login with google fail" });
         } else {
             const token = jwt.sign(Object.assign({}, req.user), secret_key);
             res.redirect(`${contains.FrontendHost}/social-auth/${token}`);
@@ -27,13 +27,13 @@ router.get('/login-google/callback',
     }
 );
 
-router.get('/login-facebook', passport.authenticate('facebook', {  session: false, scope : ['email'] }));
+router.get('/login-facebook', passport.authenticate('facebook', { session: false, scope: ['email'] }));
 router.get('/login-facebook/callback',
     passport.authenticate('facebook', {
         session: false
     }), function (req, res, next) {
         if (!req.user) {
-            res.status(401).json({message: "Login with facebook fail"});
+            res.status(401).json({ message: "Login with facebook fail" });
         } else {
             const token = jwt.sign(Object.assign({}, req.user), secret_key);
             res.redirect(`${contains.FrontendHost}/social-auth/${token}`);
@@ -51,20 +51,43 @@ router.post('/register', function (req, res, next) {
     UserModel.add(body).then((index) => {
         return res.send({ message: "Register successful." })
     }).catch((err) => {
-        console.log(err);
         var errMessage = err.sqlMessage;
         var exists = err.code.includes("ER_DUP_ENTRY");
         if (exists) {
+            UserModel.findOneEmailAndNotHaveLoginType(
+                {
+                    'email': body.email,
+                    'loginType': 'local'
+                }).then(user => {
+                    if (user && user.length > 0) {
+                        return res.status(400).send({
+                            message: "Register fail.",
+                            err: "User already exists."
+                        })
+                    } else {
+                        UserModel.udpate(body).then((index) => {
+                            return res.send({ message: "Register successful." })
+                        }).catch((err) => {
+                            console.log(err);
+                            return res.status(400).send({
+                                message: "Register fail.",
+                                err: errMessage
+                            })
+                        });
+                    }
+                }).catch(err => {
+                    console.log(err);
+                    return res.status(400).send({
+                        message: "Register fail.",
+                        err: errMessage
+                    })
+                });
+        } else {
             return res.status(400).send({
                 message: "Register fail.",
-                err: "User already exists."
+                err: errMessage
             })
         }
-
-        return res.status(400).send({
-            message: "Register fail.",
-            err: errMessage
-        })
     });
 });
 
