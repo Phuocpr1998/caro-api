a7b0a5961a8f8e0b4a1767d1b169283ed0f19863
Phước Nguyễn Văn <phuocpr1998@gmail.com>
2019-10-27 19:05:12 +0700

    add event socket io

--- socket/socketserver.js
+++ socket/socketserver.js
@@ -1,5 +1,30 @@
+const gameRooms = new Map();
+
 module.exports = (socketIo) => {
   socketIo.on('connection', function (socket) {
     console.log('a user connected');
+
+    socket.on('disconnect', function () {
+      const parner = gameRooms.get(socket.id);
+      if (parner != undefined) {
+        socketIo.to(parner).emit('disconnected');
+      }
+    });
+    
+    socket.on('message_chat', function (from, msg) {
+      console.log('Received message ', from, ' saying ', msg);
+      const parner = gameRooms.get(socket.id);
+      if (parner != undefined) {
+        socketIo.to(parner).emit('message_chat', msg);
+      }
+    });
+
+    socket.on('message_typing', function (from, msg) {
+      console.log('Received message ', from, ' saying ', msg);
+      const parner = gameRooms.get(socket.id);
+      if (parner != undefined) {
+        socketIo.to(parner).emit('message_typing', msg);
+      }
+    });
   });
 };
