33633396d480cd431c7cbe406c0a718366164957
Nguyễn Văn Phước <phuocnv2@vng.com.vn>
2019-11-06 19:07:32 +0700

    load path from config

--- config/auth.js
+++ config/auth.js
@@ -1,13 +1,14 @@
-const hostUrl = 'https://caro-api-wnc.herokuapp.com';
+const hostApiUrl = 'https://caro-api-wnc.herokuapp.com';
+
 module.exports = {
     'facebookAuth': {
         'clientID': '746985432393934',
         'clientSecret': 'a4703f641342b50d051093dcb8a884dd',
-        'callbackURL': `${hostUrl}/user/login-facebook/callback`
+        'callbackURL': `${hostApiUrl}/user/login-facebook/callback`
     },
     'googleAuth': {
         'clientID': '1001676497241-vn4rjm0l9cg75l4301iuflfu1avu9l44.apps.googleusercontent.com',
         'clientSecret': 'i9TWeJu1FRBvK0wHcIW9acA7',
-        'callbackURL': `${hostUrl}/user/login-google/callback`
+        'callbackURL': `${hostApiUrl}/user/login-google/callback`
     }
 };
